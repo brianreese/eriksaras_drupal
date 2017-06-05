9faccaac92545955a2a2b77a9268e21ba1742170
Brian Reese <Brian_R_Reese@oa.eop.gov>
2017-06-05 15:13:33 -0400

    Adding newrelic sitename to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -579,3 +579,10 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+/**
+ * Newrelic app configuration.
+ */
+if (extension_loaded('newrelic')) {
+  newrelic_set_appname("eriksarasmusic.com;PHP Application", '', 'true');
+}
